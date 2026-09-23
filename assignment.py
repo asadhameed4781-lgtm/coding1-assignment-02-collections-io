6b4d681cde0c23344b865e77c0e5370ac7b38f03
Asad Hameed <hameed.asad@student.ceu.edu>
2026-09-23 17:15:07 +0200

    add

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" # Initialize the drink variable
+sales = {"tea": 3, "coffee": 5} # Initialize the sales dictionary
+sold = sales[drink] # Get the number of sold items for the specified drink
+sold = sold + 1 # Increment the sold count
+print(drink) # Print the drink name
+print(sold) # Print the updated sold count
